fbefd822ee1a5bbc5e5514595220e3b237800666
flcsn <flacson98@gmail.com>
2022-05-02 11:07:09 +0800

    removed xl and lg breakpoints and added some size adjustments to Intro and About

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <section className='flex flex-col bg-blue-900 p-3 md:p-9 min-w-screen min-h-screen'>
+    <section className='flex flex-col bg-blue-900 p-3 sm:p-5 md:p-9 min-w-screen min-h-screen'>
       <div className='flex-grow max-w-screen-lg h-full m-auto'>
-        <h2 className='mb-1 md:mb-5 text-xl font-semibold tracking-tight text-slate-200 sm:text-3xl md:text-5xl lg:text-7xl'>About</h2>
-        <p className='text-sm text-slate-200 tracking-wide font-light sm:text-lg md:text-xl lg:text-3xl lg:leading-normal'>
+        <h2 className='mb-1 md:mb-5 text-xl font-semibold tracking-tight text-slate-200 sm:text-3xl md:text-5xl'>About</h2>
+        <p className='text-sm text-slate-200 tracking-wide font-light sm:text-lg md:text-xl md:leading-normal'>
           I take pride in creating things that other people
           can experience and enjoy. Lorem ipsum dolor sit amet,
           consectetur adipiscing elit. Sed gravida, lacus eu egestas
@@ -15,8 +15,8 @@ const About = () => {
         </p>
       </div>
       <div className='flex-grow max-w-screen-lg h-full m-auto'>
-        <h2 className='mb-1 md:mb-5 text-xl font-semibold tracking-tight text-slate-200 sm:text-3xl md:text-5xl lg:text-7xl'>A Brief History</h2>
-        <p className='text-sm text-slate-200 tracking-wide font-light sm:text-lg md:text-xl lg:text-3xl lg:leading-normal'>
+        <h2 className='mb-1 md:mb-5 text-xl font-semibold tracking-tight text-slate-200 sm:text-3xl md:text-5xl'>A Brief History</h2>
+        <p className='text-sm text-slate-200 tracking-wide font-light sm:text-lg md:text-xl md:leading-normal'>
           I started my journey into programming with
           Harvard&apos;s Intro to Computer Science (CS50). Lorem ipsum dolor sit amet,
           consectetur adipiscing elit. Sed gravida, lacus eu egestas

fd9ea0a4a66ccb8bc60dea9f54ffbd02bb02935c
flcsn <flacson98@gmail.com>
2022-05-03 17:51:57 +0800

    added actual text to About section and a temporarily  non-functional link to resume

--- src/components/About.js
+++ src/components/About.js
@@ -6,24 +6,30 @@ const About = () => {
       <div className='flex-grow max-w-screen-lg h-full m-auto'>
         <h2 className='mb-1 md:mb-5 text-xl font-semibold tracking-tight text-slate-200 sm:text-3xl md:text-5xl'>About</h2>
         <p className='text-sm text-slate-200 tracking-wide font-light sm:text-lg md:text-xl md:leading-normal'>
-          I take pride in creating things that other people
-          can experience and enjoy. Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit. Sed gravida, lacus eu egestas
-          blandit, risus ligula pellentesque nulla, id congue felis
-          velit eget neque. Interdum et malesuada fames ac ante ipsum
-          primis in faucibus.
+          I am a former HR professional who recently decided to transition to a career in web development.
+          As a developer, I take deep pride in building applications that can both address practical problems for clients,
+          and lead to satisfying experiences for end users.
+          If you would like to know more about my credentials, my resume can be downloaded
+          <a className='font-semibold' href='resume.pdf' download='FrankieLacson.pdf' target='_blank' rel='noopener noreferrer'> here</a> (PDF).
+        </p>
+        <br/>
+        <p className='text-sm text-slate-200 tracking-wide font-light sm:text-lg md:text-xl md:leading-normal'>
+          When I am not working on a project, I can be found bashing my head against the wall in Elden Ring,
+          or trying out a new Fire Emblem game while I wait for the next major FFXIV update.
         </p>
       </div>
       <div className='flex-grow max-w-screen-lg h-full m-auto'>
         <h2 className='mb-1 md:mb-5 text-xl font-semibold tracking-tight text-slate-200 sm:text-3xl md:text-5xl'>A Brief History</h2>
         <p className='text-sm text-slate-200 tracking-wide font-light sm:text-lg md:text-xl md:leading-normal'>
-          I started my journey into programming with
-          Harvard&apos;s Intro to Computer Science (CS50). Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit. Sed gravida, lacus eu egestas
-          blandit, risus ligula pellentesque nulla, id congue felis
-          velit eget neque. Interdum et malesuada fames ac ante ipsum
-          primis in faucibus. Integer consequat odio eget consequat dapibus.
-          Donec auctor urna at risus eleifend, ac cursus massa porta.
+          I started my journey into programming with Harvard&apos;s
+          <a className='font-semibold' href='https://cs50.harvard.edu' target='_blank' rel='noopener noreferrer'> Intro to Computer Science (CS50)</a> back in late-2020.
+          To help solidify my understanding of core programming concepts, I worked through University of Helsinki&apos;s
+          <a className='font-semibold' href='https://java-programming.mooc.fi/' target='_blank' rel='noopener noreferrer'> Java Programming course</a>.
+          Afterwards, I decided to learn about basic data structures and algorithms with Princeton University&apos;s
+          <a className='font-semibold' href='https://www.coursera.org/learn/algorithms-part1' target='_blank' rel='noopener noreferrer'> Algorithms</a> course.
+          Finally, I took up University of Helsinki&apos;s
+          <a className='font-semibold' href='https://fullstackopen.com/en/' target='_blank' rel='noopener noreferrer'> course on modern web development </a>
+          where I learned my current skills in JavaScript technologies.
         </p>
       </div>
     </section>

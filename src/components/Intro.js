38a9c950607f06d7d9d8f624e5bd767ae650b64a
flcsn <flacson98@gmail.com>
2022-05-04 17:52:23 +0800

    added animations to Intro and an arrow to take user to About section

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,11 +1,28 @@
 import React from 'react'
 
+import { FaArrowAltCircleDown } from 'react-icons/fa'
+
 const Intro = () => {
+  const arrowClick = () => {
+    window.scroll({
+      top: window.innerHeight,
+      left: 0,
+      behavior: 'smooth'
+    })
+  }
+
   return (
     <section className='flex bg-slate-100 min-w-screen h-screen'>
       <div className='flex flex-col m-auto text-center md:gap-5'>
-        <h1 className='text-3xl font-bold tracking-tight text-blue-600 sm:text-5xl md:text-7xl'>Hi, I&apos;m Frankie.</h1>
-        <h2 className='text-xl text-blue-900 sm:text-3xl md:text-5xl'>I&apos;m a full stack developer.</h2>
+        <h1 className='opacity-0 animate-[fadeIn_1s_ease-in-out_0s_forwards] text-3xl font-bold tracking-tight text-blue-600 sm:text-5xl md:text-7xl'>
+          Hi, I&apos;m Frankie.
+        </h1>
+        <h2 className='opacity-0 animate-[fadeIn_1s_ease-in-out_1s_forwards] text-xl text-blue-900 sm:text-3xl md:text-5xl'>
+          I&apos;m a full stack developer.
+        </h2>
+        <button className='opacity-0 animate-[fadeIn_1s_ease-in-out_2s_forwards] mt-10 mx-auto h-6 w-6' onClick={arrowClick}>
+          <FaArrowAltCircleDown id='downArrow' className='h-full w-full animate-bounce fill-blue-900 hover:fill-blue-600' />
+        </button>
       </div>
     </section>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,24 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeIn: 'fadeIn',
+        fadeOut: 'fadeOut',
+        'bounce-1/2': 'bounce 0.5s linear'
+      },
+
+      keyframes: theme => ({
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 100 }
+        },
+        fadeOut: {
+          '0%': { opacity: 100 },
+          '100%': { opacity: 0 }
+        },
+      })
+    },
   },
   plugins: [],
 }
